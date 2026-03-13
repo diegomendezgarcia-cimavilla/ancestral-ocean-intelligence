b965488055f1cc30c975dc1e9594632f544c4a53
diegomendezgarcia-cimavilla <diegomendezgarcia@gmail.com>
2026-03-13 23:44:06 +0100

    Create community_insights.py

---
+++ community_insights.py
@@ -0,0 +1,7 @@
+def community_message():
+    return """
+Cada registro ayuda a entender mejor cómo los ritmos naturales
+afectan el bienestar humano.
+
+Este proyecto busca generar conocimiento abierto para todos.
+"""
