3fb0a450215875d10a19c2b15171d91ad510be07
diegomendezgarcia-cimavilla <diegomendezgarcia@gmail.com>
2026-03-13 23:47:15 +0100

    Create ancestral_knowledge.py

---
+++ ancestral_knowledge.py
@@ -0,0 +1,31 @@
+knowledge = {
+
+"rest":[
+"siesta corta",
+"caminar en naturaleza",
+"infusión de hierbas",
+"respiración profunda"
+],
+
+"energy":[
+"actividad física",
+"comida natural",
+"exposición al sol",
+"agua fresca"
+],
+
+"creativity":[
+"escribir ideas",
+"música",
+"arte",
+"paseo tranquilo"
+],
+
+"balance":[
+"comer alimentos frescos",
+"reducir estímulos digitales",
+"meditación",
+"contacto con agua o mar"
+]
+
+}
